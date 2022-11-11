7cc95e9ef14715201d51546fb7a8a16a7266b5d0
M. Delgado-Ortet <md863@cam.ac.uk>
2022-11-11 19:17:25 +0000

    MODIFY: run.py sorted libraries using isort.

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import math
 import shutil
 from datetime import datetime
+
 import matplotlib.pyplot as plt
 import numpy as np
 from solid import *
+
 from utils.import_functions import *
 from utils.manipulate_dicom_functions import *
 from utils.mould_modelling_functions import *
