7af18f96d7cf0e3eaf4127a4159cfd64af16dad2
M. Delgado-Ortet <md863@cam.ac.uk>
2022-11-12 23:22:20 +0000

    Added a import_functions test to check missing dicom info.

--- tests/test_import_functions.py
+++ tests/test_import_functions.py
@@ -47,3 +47,9 @@ def test_type_dicom_info():
 def test_path_dicom_info():
     with raises(OSError) as exception:
         import_yaml('path_dicom_info.yaml', check_dicom_info)
+
+def missing_dicom_info():
+    os.mkdir('tmp_path') #  To prevent OSError to be raised.
+    with raises(KeyError) as exception:
+        import_yaml('missing_dicom_info.yaml', check_dicom_info)
+    os.rmdir('tmp_path')
