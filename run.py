b4747dff22666ce1d07594ff2d72f52976e29b1d
M. Delgado-Ortet <md863@cam.ac.uk>
2022-11-13 00:03:15 +0000

    Added argument parser and the import inputs section.

--- run.py
+++ run.py
@@ -11,3 +11,61 @@ from utils.import_functions import *
 from utils.manipulate_dicom_functions import *
 from utils.mould_modelling_functions import *
 from utils.tumour_modelling_function import *
+
+if __name__ == '__main__':
+    #%% ----------------------------------------
+    # ARGUMENT PARSER
+    # ------------------------------------------
+    parser = argparse.ArgumentParser(description = "Let's build a mould!")
+    parser.add_argument('mould_id', type = str)
+    parser.add_argument('--tunable_parameters', type = str, default = 'tunable_parameters.yaml')
+    parser.add_argument('--dicom_info', type = str, default = 'dicom_info.yaml')
+    parser.add_argument('--results_path', type = str, default = 'results', help = "Path to the folder where to save the results. A subfolder under the mould_id name will be created.")
+    
+    args = parser.parse_args()
+        
+    
+    #%% ----------------------------------------
+    # 1. IMPORT INPUTS
+    # ------------------------------------------
+    print("# ------------------------------------------ \n# 1. IMPORT INPUTS \n# ------------------------------------------")
+
+    print("\t## Importing " + args.tunable_parameters + "...", end = "")
+    globals().update(import_yaml(args.tunable_parameters, check_tunable_parameters))
+    print(' OK')
+
+    print("\t## Importing " + args.dicom_info + "...", end = "")
+    dicom_info_dict = import_yaml(args.dicom_info, check_dicom_info)
+    print(' OK')
+
+    print('\t## Extracting VOIs...', end = "")
+    tumour_mask, base_mask, ref_point_1_mask, ref_point_2_mask = get_roi_masks(dicom_info_dict)
+    print(' OK')
+    
+    path_to_results = args.results_path
+    mould_id = args.mould_id
+    if not os.path.isdir(path_to_results):
+        os.mkdir(path_to_results)
+    
+    dst_dir = os.path.join(path_to_results, mould_id)
+    try:
+        os.mkdir(dst_dir)
+
+    except FileExistsError:
+        now = datetime.now()
+        date_time = now.strftime("%Y%m%d_%H%M%S")
+        mould_id = mould_id + '_' + date_time
+        new_dst_dir = os.path.join(path_to_results, mould_id)
+        print('WARNING: ' + dst_dir + ' already exists. Creating ' + new_dst_dir + ' instead.')
+        
+        dst_dir = new_dst_dir
+        os.mkdir(dst_dir)
+        
+    print('Saving imported yaml files to ' + os.path.join(dst_dir, 'yaml_inputs') + '...', end = "")
+    os.mkdir(os.path.join(dst_dir, 'yaml_inputs'))
+    shutil.copyfile(args.tunable_parameters, os.path.join(dst_dir, 'yaml_inputs', 'tunable_parameters.yaml'))
+    shutil.copyfile(args.dicom_info, os.path.join(dst_dir, 'yaml_inputs', 'dicom_info.yaml'))
+
+    print(' OK')
+
+    print('Import complete.')
