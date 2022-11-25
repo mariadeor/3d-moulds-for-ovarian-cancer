5c4a3ca22d710cf4a5728268e27099fb2059d410
M. Delgado-Ortet <md863@cam.ac.uk>
2022-11-25 16:27:34 +0000

    MODIFY: Sorted libraries using isort.

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
-from solid import *
 from skimage.draw import polygon2mask
+from solid import *
 
 from utils.import_functions import *
 from utils.manipulate_dicom_functions import *
