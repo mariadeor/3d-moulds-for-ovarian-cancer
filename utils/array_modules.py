6b08afb25e5b0a10954645f6a0615a873b9d29ab
M. Delgado-Ortet <md863@cam.ac.uk>
2023-02-23 13:55:27 +0100

    MODIFY: Alphabetical order, black style, commented.

--- utils/array_modules.py
+++ utils/array_modules.py
@@ -1,48 +1,67 @@
-import os
+#####################################################################
+# AUTHOR        Maria Delgado-Ortet
+# CONTACT       md863@cam.ac.uk
+# INSTITUTION   Department of Radiology, University of Cambridge
+# DATE          Nov 2022 - Feb 2023
+#####################################################################
+"""Functions to manipulate numpy arrays."""
 
+#%% -----------------LIBRARIES--------------
 import numpy as np
-import pydicom
 import scipy.ndimage
-from rt_utils import RTStruct, RTStructBuilder
 
-from utils.display_functions import (
-    plot_slices,
-)
+from utils.display_functions import plot_scan_seg_slices
 
-def reslice(array, scale_x, scale_y, scale_z):
+
+#%% -----------------FUNCTIONS--------------
+def build_label_mask(*masks):
     """
-    This function reslices and interpolates the input array to
-    (1, 1, 1) mm voxel size using zero-order spline interpolation.
+    This function combines different masks into a numeric label mask.
+    Each mask is labelled according to its position in the input list (starting from 1).
+    They are combined in reversed order, i.e. if some voxels have more than two labels, they will keep the lowest one.
         INPUTS:
-            array <numpy.ndarray>:  Array to reslice and interpolate.
-            scale_x, scale_y, scale_z <float>:  Original DICOM images
-                                                voxel size in mm.
+            masks <numpy.ndarray>:      Boolean arrays to combine.
         OUTPUTS:
-            array <numpy.ndarray>:  Resliced and interpolated input
-                                    array to (1, 1, 1) mm voxel size.
+            label_mask <numpy.ndarray>: Numeric label mask as float array.
     """
-
-    return scipy.ndimage.zoom(
-        array, (scale_x, scale_y, scale_z), order=0, prefilter=False
-    )
-
-
-def get_centroid(mask, val=True):
-    """
-    This function returns the centroid of the pixels equal to a specified
-    value.
-    If a value is not specified, the function assumes by default that it
-    is a boolean array and looks for the centroid of the True pixels.
-        INPUTS:
-            mask <numpy.ndarray>:   Array.
-            val (optional):         Value of the pixels to find the centroid of.
-        OUTPUTS:
-            y, x, z <float>: Centroid coordinates.
-    """
-
-    idx = np.where(mask == val)
     
-    return np.array([np.mean(idx[0]), np.mean(idx[1]), np.mean(idx[2])])
+    from inputs import display
+
+    scan_sz = np.shape(masks[0])
+    label_mask = np.zeros(scan_sz)
+    for idx, mask in reversed(list(enumerate(masks))):
+        label_mask[mask] = idx + 1
+
+    if (
+        display
+    ):  # OPT: Add "--display" to the command line to display resliced rois_combined.
+        print("Displaying imported VOIs... ", end="")
+        tumour_slices = np.unique(np.argwhere(label_mask == 1)[:, 2])
+        plot_scan_seg_slices(label_mask, tumour_slices, "Imported and re-sliced VOIs")
+
+    return label_mask
+
+
+def crop_voi(label_mask, val=1):
+    """
+    This function crops the VOI of interest of a label mask.
+        INPUTS:
+            label_mask <numpy.ndarray>: Numeric label mask to rotate.
+            val <int>:                  Value of the VOI of interest in the label_mask.
+        OUTPUTS:
+            voi <numpy.ndarray>:        Boolean mask of the VOI of interest
+    """
+
+    # Create a boolean array with VOI of interest only:
+    label_mask_sz = np.shape(label_mask)
+    voi = np.zeros(label_mask_sz)
+    voi[label_mask == val] = 1
+
+    # Crop the VOI:
+    rmin, rmax, cmin, cmax, zmin, zmax = get_box(voi)
+    voi = voi[rmin : rmax + 1, cmin : cmax + 1, zmin : zmax + 1]
+
+    return voi
 
 
 def get_box(mask):
@@ -67,65 +86,58 @@ def get_box(mask):
     return rmin, rmax, cmin, cmax, zmin, zmax
 
 
-def build_label_mask(*masks):
+def get_centroid(mask, val=True):
     """
-    This function combines different masks into a numeric label mask.
-    Each mask is labelled according to its position in the input list (starting from 1).
-    They are combined in reversed order, i.e. if some voxels have more than two labels, they will keep the lowest one.
+    This function returns the centroid of the pixels equal to a specified
+    value.
+    If a value is not specified, the function assumes by default that it
+    is a boolean array and looks for the centroid of the True pixels.
         INPUTS:
-            masks <numpy.ndarray>:      Boolean arrays to combine.
+            mask <numpy.ndarray>:   Array.
+            val (optional):         Value of the pixels to find the centroid of.
         OUTPUTS:
-            label_mask <numpy.ndarray>: Numeric label mask as float array.
+            y, x, z <float>: Centroid coordinates.
     """
-    from  config import display
-    
-    scan_sz = np.shape(masks[0])
-    label_mask = np.zeros(scan_sz)
-    for idx, mask in reversed(list(enumerate(masks))):
-        label_mask[mask] = idx + 1
-    
-    if display: # OPT: Add "--display" to the command line to display resliced rois_combined.
-        print("Displaying imported VOIs... ", end="")
-        plot_slices(rois_combined, tumour_slices, "Imported and re-sliced VOIs")
 
-    return label_mask
+    idx = np.where(mask == val)
 
-def get_dicom_slices_idx(mask):
+    return np.array([np.mean(idx[0]), np.mean(idx[1]), np.mean(idx[2])])
+
+
+def get_mask_slices_dicom_idx(mask):
     """
     This function returns the position of the DICOM slices where the input mask is.
         INPUTS:
-            mask <numpy.ndarray>:   Boolean array.
+            mask <numpy.ndarray>:           Boolean array.
         OUTPUTS:
-            dicom_slices <numpy.ndarray>:  Array with the DICOM slice indices of the mask.
+            dicom_slices <numpy.ndarray>:   Array with the DICOM slice indices of the mask.
     """
 
-    dicom_slices = np.unique(np.argwhere(mask)[:, 2])
-    dicom_slices = np.shape(mask)[2] - dicom_slices  # DICOM slices are numbered in reverse.
+    mask_slices = np.unique(np.argwhere(mask)[:, 2])
+    dicom_slices = (
+        np.shape(mask)[2] - mask_slices
+    )  # DICOM slices are numbered in reverse.
     dicom_slices = np.sort(dicom_slices)
-    
+
     return dicom_slices
 
-def crop_voi(label_mask, val=1):
+
+def reslice(array, scale_x, scale_y, scale_z):
     """
-    This function crops the VOI of interest of a label mask.
+    This function reslices and interpolates the input array to
+    (1, 1, 1) mm voxel size using zero-order spline interpolation.
         INPUTS:
-            label_mask <numpy.ndarray>: Numeric label mask to rotate.
-            val <int>:                  Value of the VOI of interest in the label_mask.
+            array <numpy.ndarray>:  Array to reslice and interpolate.
+            scale_x, scale_y, scale_z <float>:  Original DICOM images
+                                                voxel size in mm.
         OUTPUTS:
-            voi <numpy.ndarray>:        Boolean mask of the VOI of interest
+            array <numpy.ndarray>:  Resliced and interpolated input
+                                    array to (1, 1, 1) mm voxel size.
     """
 
-    # Create a boolean array with VOI of interest only:
-    label_mask_sz = np.shape(label_mask)
-    voi = np.zeros(label_mask_sz)
-    voi[label_mask == val] = 1
-
-    # Crop the VOI:
-    rmin, rmax, cmin, cmax, zmin, zmax = get_box(voi)
-    voi = voi[rmin : rmax + 1, cmin : cmax + 1, zmin : zmax + 1]
-
-    return voi
-
+    return scipy.ndimage.zoom(
+        array, (scale_x, scale_y, scale_z), order=0, prefilter=False
+    )
 
 
 def rotate_label_mask(label_mask, theta, **kwargs):
@@ -134,25 +146,18 @@ def rotate_label_mask(label_mask, theta, **kwargs):
         INPUTS:
             label_mask <numpy.ndarray>: Numeric label mask to rotate.
             theta <float>:              Degrees to rotate the label_mask on the xy plane.
-            slice_idx_to_rotate <list>: Optional input list with the slices to rotate.
             crop_tumour <bool>:         Optional boolean input to crop the tumour. It assumes is labelled as 1.
         OUTPUTS:
             label_mask_rotated <numpy.ndarray>: Rotated input label mask.
     """
-    
-    if "slice_idx_to_rotate" in kwargs:
-        slice_idx_to_rotate = kwargs["slice_idx_to_rotate"]
-    else:
-        try:
-            from config import tumour_slices
-            slice_idx_to_rotate = tumour_slices
-        except ImportError:
-            slice_idx_to_rotate = list(range(np.shape(label_mask)[2]))
-    
+
+    from inputs import display
+
+    slice_idx_to_rotate = np.unique(np.argwhere(label_mask == 1)[:, 2])
     nbr_tumour_slices = len(slice_idx_to_rotate)
     scan_sz = np.shape(label_mask)
     label_mask_rotated = np.zeros([scan_sz[0], scan_sz[1], nbr_tumour_slices])
-    
+
     # Rotate each slice theta degrees:
     print(
         "\t## Rotating the tumour VOI %f degrees on the DICOM axial plane..." % theta,
@@ -167,7 +172,7 @@ def rotate_label_mask(label_mask, theta, **kwargs):
     # In case the rotation resulted in the base being on top (i.e. the y
     # coordinate centroid of the base is greater than the one for the tumour),
     # let's add 180 degrees rotation extra:
-    if (get_centroid(label_mask_rotated, 1)[0] > get_centroid(label_mask_rotated, 4)[0]):
+    if get_centroid(label_mask_rotated, 1)[0] > get_centroid(label_mask_rotated, 4)[0]:
         print(
             "\t## Rotating the the tumour VOI extra 180 degrees on the DICOM axial plane...",
             end="",
@@ -177,16 +182,16 @@ def rotate_label_mask(label_mask, theta, **kwargs):
                 label_mask_rotated[:, :, z], 180, reshape=False, order=0
             )
         print(" OK")
-    
-    from config import display
-    if display: # OPT: Add "--display" to the command line to display the rotated VOIs.
+
+    if display:  # OPT: Add "--display" to the command line to display the rotated VOIs.
         print("\t\tDisplaying rotated VOIs... ", end="")
-        plot_slices(rois_combined_rotated, range(nbr_tumour_slices), "Rotated VOIs")
-    
+        plot_scan_seg_slices(
+            label_mask_rotated, range(nbr_tumour_slices), "Rotated VOIs"
+        )
+
     if "crop_tumour" in kwargs and kwargs["crop_tumour"]:
         print(
-            "\t## Cropping the tumour bounding box...",
-            end="",
+            "\t## Cropping the tumour bounding box...", end="",
         )
         label_mask_rotated = crop_voi(label_mask_rotated)
         print(" OK")
