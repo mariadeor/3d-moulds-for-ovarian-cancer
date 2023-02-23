d8f57a6fa1d3c0f762edc8b938aeed7ea8596e99
mariadeor <41467566+mariadeor@users.noreply.github.com>
2023-02-23 19:06:34 +0100

    Delete write_config_functions.py

--- utils/write_config_functions.py
+++
@@ -1,7 +0,0 @@
-def dump_vars_to_config(vars_dict, mode="a"):
-    with open("config.py", mode) as f:
-        for key, value in vars_dict.items():
-            if isinstance(value, str):
-                f.write(f'{key} = "{value}"\n')
-            else:
-                f.write(f'{key} = {value}\n')
