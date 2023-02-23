62ebab2056a0909455173bc71746231df2373b88
M. Delgado-Ortet <md863@cam.ac.uk>
2023-02-23 12:45:14 +0100

    MODIFY: Split the functions in manipulate_dicom_functions into dicom_modules and array_modules.py

--- utils/array_modules.py
+++ utils/array_modules.py
@@ -1,13 +1,3 @@
-#####################################################################
-# AUTHOR        Maria Delgado-Ortet
-# CONTACT       md863@cam.ac.uk
-# INSTITUTION   Department of Radiology, University of Cambridge
-# DATE          Nov 2022
-#####################################################################
-"""Functions for manipulate DICOM files and the masks that derive
-from their ROIs."""
-
-#%% -----------------LIBRARIES--------------
 import os
 
 import numpy as np
@@ -18,61 +8,6 @@ from rt_utils import RTStruct, RTStructBuilder
 from utils.display_functions import (
     plot_slices,
 )
-
-
-#%% -----------------FUNCTIONS--------------
-def get_roi_masks(do_reslicing=False):
-    """
-    This function returns DICOM ROIs as Numpy boolean arrays.
-        INPUTS:
-            do_reslicing <bool>:    Boolean flag to reslice the ROIs to (1, 1, 1) mm voxel size.
-        OUTPUTS:
-            roi_masks <numpy.ndarray>:  Boolean array of the ROIs.
-    """
-    from config import path_to_dicom, tumour_roi_name, base_roi_name, ref_point_1_roi_name, ref_point_2_roi_name
-
-    # Find the DICOM-RT
-    for dcmfile in os.listdir(path_to_dicom):
-        if not dcmfile.startswith("."):
-            dcmfile_path = os.path.join(path_to_dicom, dcmfile)
-            ds = pydicom.dcmread(dcmfile_path)
-            if ds.Modality == "RTSTRUCT":
-                rt_struct_path = dcmfile_path
-                break
-
-    # Get the masks
-    rt_struct = RTStructBuilder.create_from(
-                  dicom_series_path=path_to_dicom,
-                  rt_struct_path=rt_struct_path
-                )
-    try:
-        tumour_mask          = rt_struct.get_roi_mask_by_name(tumour_roi_name)
-        base_mask            = rt_struct.get_roi_mask_by_name(base_roi_name)
-        ref_point_1_mask     = rt_struct.get_roi_mask_by_name(ref_point_1_roi_name)
-        ref_point_2_mask     = rt_struct.get_roi_mask_by_name(ref_point_2_roi_name)
-    
-    except RTStruct.ROIException:
-        print(
-            "\nERROR! Specified ROI(s) do not exist in the DICOM-RT file, which contains: ",
-            rt_struct.get_roi_names(),
-        )
-        raise SystemExit(0)
-    
-    if do_reslicing:
-        print("\t## Re-slicing VOIs to voxel size (1, 1, 1) mm...", end="")
-        scale_x, scale_y, scale_z = get_dicom_voxel_size(path_to_dicom)
-
-        tumour_mask = reslice(tumour_mask, scale_x, scale_y, scale_z)
-        base_mask = reslice(base_mask, scale_x, scale_y, scale_z)
-        ref_point_1_mask = reslice(ref_point_1_mask, scale_x, scale_y, scale_z)
-        ref_point_2_mask = reslice(ref_point_2_mask, scale_x, scale_y, scale_z)
-        print(" OK")
-        print("\t\tOriginal voxel size: (%f, %f, %f) mm" % (scale_x, scale_y, scale_z))
-
-        print("Re-slicing complete.")
-
-    return tumour_mask, base_mask, ref_point_1_mask, ref_point_2_mask
-
 
 def reslice(array, scale_x, scale_y, scale_z):
     """
@@ -132,48 +67,6 @@ def get_box(mask):
     return rmin, rmax, cmin, cmax, zmin, zmax
 
 
-def get_dicom_slices_idx(mask):
-    """
-    This function returns the position of the DICOM slices where the input mask is.
-        INPUTS:
-            mask <numpy.ndarray>:   Boolean array.
-        OUTPUTS:
-            dicom_slices <numpy.ndarray>:  Array with the DICOM slice indices of the mask.
-    """
-
-    dicom_slices = np.unique(np.argwhere(mask)[:, 2])
-    dicom_slices = np.shape(mask)[2] - dicom_slices  # DICOM slices are numbered in reverse.
-    dicom_slices = np.sort(dicom_slices)
-    
-    return dicom_slices
-
-
-def get_dicom_voxel_size(path_to_dicom):
-    """
-    This function returns the voxel size of the input DICOM series.
-        INPUTS:
-            path_to_dicom <str>:    Path to DICOM series.
-        OUTPUTS:
-            x, y, z <float>:        Voxel size.
-    """
-
-    ds = pydicom.dcmread(
-        os.path.join(path_to_dicom, os.listdir(path_to_dicom)[1])
-    )  # Read DICOM metadata.
-    x = ds.PixelSpacing[0]
-    y = ds.PixelSpacing[1]
-    try:
-        z = (
-            ds.SpacingBetweenSlices
-            if ds.SliceThickness > ds.SpacingBetweenSlices
-            else ds.SliceThickness
-        )
-    except AttributeError:
-        z = ds.SliceThickness
-    
-    return x, y, z
-
-
 def build_label_mask(*masks):
     """
     This function combines different masks into a numeric label mask.
@@ -196,6 +89,43 @@ def build_label_mask(*masks):
         plot_slices(rois_combined, tumour_slices, "Imported and re-sliced VOIs")
 
     return label_mask
+
+def get_dicom_slices_idx(mask):
+    """
+    This function returns the position of the DICOM slices where the input mask is.
+        INPUTS:
+            mask <numpy.ndarray>:   Boolean array.
+        OUTPUTS:
+            dicom_slices <numpy.ndarray>:  Array with the DICOM slice indices of the mask.
+    """
+
+    dicom_slices = np.unique(np.argwhere(mask)[:, 2])
+    dicom_slices = np.shape(mask)[2] - dicom_slices  # DICOM slices are numbered in reverse.
+    dicom_slices = np.sort(dicom_slices)
+    
+    return dicom_slices
+
+def crop_voi(label_mask, val=1):
+    """
+    This function crops the VOI of interest of a label mask.
+        INPUTS:
+            label_mask <numpy.ndarray>: Numeric label mask to rotate.
+            val <int>:                  Value of the VOI of interest in the label_mask.
+        OUTPUTS:
+            voi <numpy.ndarray>:        Boolean mask of the VOI of interest
+    """
+
+    # Create a boolean array with VOI of interest only:
+    label_mask_sz = np.shape(label_mask)
+    voi = np.zeros(label_mask_sz)
+    voi[label_mask == val] = 1
+
+    # Crop the VOI:
+    rmin, rmax, cmin, cmax, zmin, zmax = get_box(voi)
+    voi = voi[rmin : rmax + 1, cmin : cmax + 1, zmin : zmax + 1]
+
+    return voi
+
 
 
 def rotate_label_mask(label_mask, theta, **kwargs):
@@ -262,25 +192,3 @@ def rotate_label_mask(label_mask, theta, **kwargs):
         print(" OK")
 
     return label_mask_rotated
-
-
-def crop_voi(label_mask, val=1):
-    """
-    This function crops the VOI of interest of a label mask.
-        INPUTS:
-            label_mask <numpy.ndarray>: Numeric label mask to rotate.
-            val <int>:                  Value of the VOI of interest in the label_mask.
-        OUTPUTS:
-            voi <numpy.ndarray>:        Boolean mask of the VOI of interest
-    """
-
-    # Create a boolean array with VOI of interest only:
-    label_mask_sz = np.shape(label_mask)
-    voi = np.zeros(label_mask_sz)
-    voi[label_mask == val] = 1
-
-    # Crop the VOI:
-    rmin, rmax, cmin, cmax, zmin, zmax = get_box(voi)
-    voi = voi[rmin : rmax + 1, cmin : cmax + 1, zmin : zmax + 1]
-
-    return voi
---
+++ utils/dicom_modules.py
@@ -0,0 +1,95 @@
+#####################################################################
+# AUTHOR        Maria Delgado-Ortet
+# CONTACT       md863@cam.ac.uk
+# INSTITUTION   Department of Radiology, University of Cambridge
+# DATE          Nov 2022
+#####################################################################
+"""Functions to get ROIs and metadata information from DICOM files."""
+
+#%% -----------------LIBRARIES--------------
+import os
+
+import numpy as np
+import pydicom
+from rt_utils import RTStruct, RTStructBuilder
+
+
+#%% -----------------FUNCTIONS--------------
+def get_roi_masks(do_reslicing=False):
+    """
+    This function returns DICOM ROIs as Numpy boolean arrays.
+        INPUTS:
+            do_reslicing <bool>:    Boolean flag to reslice the ROIs to (1, 1, 1) mm voxel size.
+        OUTPUTS:
+            roi_masks <numpy.ndarray>:  Boolean arrays of the ROIs.
+    """
+    
+    from inputs import path_to_dicom, tumour_roi_name, base_roi_name, ref_point_1_roi_name, ref_point_2_roi_name
+
+    # Find the DICOM-RT
+    for dcmfile in os.listdir(path_to_dicom):
+        if not dcmfile.startswith("."):
+            dcmfile_path = os.path.join(path_to_dicom, dcmfile)
+            ds = pydicom.dcmread(dcmfile_path)
+            if ds.Modality == "RTSTRUCT":
+                rt_struct_path = dcmfile_path
+                break
+
+    # Get the masks
+    rt_struct = RTStructBuilder.create_from(
+                  dicom_series_path=path_to_dicom,
+                  rt_struct_path=rt_struct_path
+                )
+    try:
+        tumour_mask          = rt_struct.get_roi_mask_by_name(tumour_roi_name)
+        base_mask            = rt_struct.get_roi_mask_by_name(base_roi_name)
+        ref_point_1_mask     = rt_struct.get_roi_mask_by_name(ref_point_1_roi_name)
+        ref_point_2_mask     = rt_struct.get_roi_mask_by_name(ref_point_2_roi_name)
+    
+    except RTStruct.ROIException:
+        print(
+            "\nERROR! Specified ROI(s) do not exist in the DICOM-RT file, which contains: ",
+            rt_struct.get_roi_names(),
+        )
+        raise SystemExit(0)
+    
+    if do_reslicing:
+        print("\t## Re-slicing VOIs to voxel size (1, 1, 1) mm...", end="")
+        scale_x, scale_y, scale_z = get_dicom_voxel_size(path_to_dicom)
+
+        tumour_mask = reslice(tumour_mask, scale_x, scale_y, scale_z)
+        base_mask = reslice(base_mask, scale_x, scale_y, scale_z)
+        ref_point_1_mask = reslice(ref_point_1_mask, scale_x, scale_y, scale_z)
+        ref_point_2_mask = reslice(ref_point_2_mask, scale_x, scale_y, scale_z)
+        print(" OK")
+        print("\t\tOriginal voxel size: (%f, %f, %f) mm" % (scale_x, scale_y, scale_z))
+
+        print("Re-slicing complete.")
+
+    return tumour_mask, base_mask, ref_point_1_mask, ref_point_2_mask
+
+
+def get_dicom_voxel_size(path_to_dicom):
+    """
+    This function returns the voxel size of the input DICOM series.
+        INPUTS:
+            path_to_dicom <str>:    Path to DICOM series.
+        OUTPUTS:
+            x, y, z <float>:        Voxel size.
+    """
+
+    ds = pydicom.dcmread(
+        os.path.join(path_to_dicom, os.listdir(path_to_dicom)[1])
+    )  # Read DICOM metadata.
+    x = ds.PixelSpacing[0]
+    y = ds.PixelSpacing[1]
+    try:
+        z = (
+            ds.SpacingBetweenSlices
+            if ds.SliceThickness > ds.SpacingBetweenSlices
+            else ds.SliceThickness
+        )
+    except AttributeError:
+        z = ds.SliceThickness
+    
+    return x, y, z
