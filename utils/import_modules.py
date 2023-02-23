57de1e45a487df39fc273acba514a39ad6171ef4
M. Delgado-Ortet <md863@cam.ac.uk>
2023-02-23 12:19:46 +0100

    MODIFY: Alphabetical order, black style, documented.

--- utils/import_modules.py
+++ utils/import_modules.py
@@ -17,9 +17,13 @@ from utils.export_modules import create_dst_dir
 
 
 #%% -----------------FUNCTIONS--------------
-def build_parser():
+def build_parser(output_filename="inputs.py"):
     """
-    This function builds the parser for the main code to build the 3D moulds.
+    This function builds the parser for the main code to build the 3D moulds and saves
+    the variables to a Python script.
+        INPUTS:
+            output_filename <str>:  Name of the Python filename to save the
+                                    imported variables.
         OUTPUTS:
             args <argparse.Namespace>:  Object that contains all the data in the parser.
     """
@@ -72,78 +76,9 @@ def build_parser():
     )
 
     args = parser.parse_args()
-    dump_vars_to_file(vars(args), "inputs.py" ,mode="w")
-
+    dump_vars_to_file(vars(args), "inputs.py", mode="w")
+    
     return args
-
-
-def import_yaml(path_to_file, check_func, output_filename="inputs.py"):
-    """
-    This function saves the variables defined in a yaml file to a Python file.
-    The suitability of the variables is tested through an inputted function.
-        INPUTS:
-            path_to_file <str>:     Path to input yaml file.
-            check_func <function>:  Function to check that the variables
-                                    defined in the file are the expected
-                                    and suitable.
-    """
-
-    with open(path_to_file) as yaml_file:
-        yaml_dict = yaml.safe_load(yaml_file)
-
-    check_func(yaml_dict)
-    dump_vars_to_file(yaml_dict, output_filename)
-
-
-def check_tunable_parameters(tunable_parameters):
-    """
-    This function checks the yaml file containing the tunable parameters
-    and raises errors if unsuitable.
-        INPUTS:
-            tunable_parameters <dict>:  Dictionary containing the tunable
-                                        parameters defined in the input
-                                        yaml file.
-    """
-
-    for key, item in tunable_parameters.items():  # Loop through each defined parameter
-        if not item:  # Check if the parameter is empty
-            raise ValueError(key + " is empty.")
-
-        elif type(item) != int and type(item) != float:  # Check the parameter type
-            raise TypeError(key + " is <" + type(item).__name__ + "> and should be *<int>* or *<float>*.")
-
-        elif item < 0:  # Check if positive
-            raise ValueError(key + " should be *positive*.")
-
-    # Check if there are missing or unrecognised (extra) parameters:
-    ## List with all the *expected* tunable parameters:
-    required_params = [
-        "slice_thickness",
-        "slit_thickness",
-        "guides_thickness",
-        "cavity_wall_thickness",
-        "dist_orguide_baseplate",
-        "baseplate_height",
-        "baseplate_xy_offset",
-        "cavity_height_pct",
-        "slguide_height_offset",
-        "depth_orslit",
-    ]
-    ## List with all the the *inputted* parameters:
-    input_params = tunable_parameters.keys()
-
-    ## Check if there are any missing parameters
-    missing_params = [param for param in required_params if param not in set(input_params)]
-    if missing_params:
-        raise KeyError("Missing: " + " ".join(missing_params))
-
-    ## Check if there are any unrecognised parameters
-    ## ATTN: If so, it prints a warning but it does not raise any error.
-    ## In case a parameter had a typo, an error would have already been
-    ## raised as it would have been identified as a missing parameter above.
-    unrecognised_params = [param for param in input_params if param not in set(required_params)]
-    if unrecognised_params:
-        print("WARNING: Tunable parameter(s) " + " ".join(unrecognised_params) + " are unrecognised and will be ignored.")
 
 
 def check_dicom_info(dicom_info):
@@ -160,7 +95,14 @@ def check_dicom_info(dicom_info):
             raise ValueError(key + " is empty.")
 
         elif type(item) != str:  # Check the specificity type
-            raise TypeError(key + " is <" + type(item).__name__ + "> and should be *<str>*. Adding quotes to " + key + " may resolve this issue.")
+            raise TypeError(
+                key
+                + " is <"
+                + type(item).__name__
+                + "> and should be *<str>*. Adding quotes to "
+                + key
+                + " may resolve this issue."
+            )
 
     # Check if path_to_dicom is an existing directory.
     if not os.path.isdir(dicom_info["path_to_dicom"]):
@@ -189,4 +131,92 @@ def check_dicom_info(dicom_info):
     ## raised as it would have been identified as a missing info bit above.
     unrecognised_info = [info for info in input_info if info not in set(required_info)]
     if unrecognised_info:
-        print("WARNING: DICOM information bit(s) " + " ".join(unrecognised_info) + " are unrecognised and will be ignored.")
+        print(
+            "WARNING: DICOM information bit(s) "
+            + " ".join(unrecognised_info)
+            + " are unrecognised and will be ignored."
+        )
+
+
+def check_tunable_parameters(tunable_parameters):
+    """
+    This function checks the yaml file containing the tunable parameters
+    and raises errors if unsuitable.
+        INPUTS:
+            tunable_parameters <dict>:  Dictionary containing the tunable
+                                        parameters defined in the input
+                                        yaml file.
+    """
+
+    for key, item in tunable_parameters.items():  # Loop through each defined parameter
+        if not item:  # Check if the parameter is empty
+            raise ValueError(key + " is empty.")
+
+        elif type(item) != int and type(item) != float:  # Check the parameter type
+            raise TypeError(
+                key
+                + " is <"
+                + type(item).__name__
+                + "> and should be *<int>* or *<float>*."
+            )
+
+        elif item < 0:  # Check if positive
+            raise ValueError(key + " should be *positive*.")
+
+    # Check if there are missing or unrecognised (extra) parameters:
+    ## List with all the *expected* tunable parameters:
+    required_params = [
+        "slice_thickness",
+        "slit_thickness",
+        "guides_thickness",
+        "cavity_wall_thickness",
+        "dist_orguide_baseplate",
+        "baseplate_height",
+        "baseplate_xy_offset",
+        "cavity_height_pct",
+        "slguide_height_offset",
+        "depth_orslit",
+    ]
+    ## List with all the the *inputted* parameters:
+    input_params = tunable_parameters.keys()
+
+    ## Check if there are any missing parameters
+    missing_params = [
+        param for param in required_params if param not in set(input_params)
+    ]
+    if missing_params:
+        raise KeyError("Missing: " + " ".join(missing_params))
+
+    ## Check if there are any unrecognised parameters
+    ## ATTN: If so, it prints a warning but it does not raise any error.
+    ## In case a parameter had a typo, an error would have already been
+    ## raised as it would have been identified as a missing parameter above.
+    unrecognised_params = [
+        param for param in input_params if param not in set(required_params)
+    ]
+    if unrecognised_params:
+        print(
+            "WARNING: Tunable parameter(s) "
+            + " ".join(unrecognised_params)
+            + " are unrecognised and will be ignored."
+        )
+
+
+def import_yaml(path_to_file, check_func, output_filename="inputs.py"):
+    """
+    This function saves the variables defined in a yaml file to a Python file.
+    The suitability of the variables is tested through an inputted function.
+        INPUTS:
+            path_to_file <str>:     Path to input yaml file.
+            check_func <function>:  Function to check that the variables
+                                    defined in the file are the expected
+                                    and suitable.
+            output_filename <str>:  Name of the Python filename to save the
+                                    imported variables.
+    """
+
+    with open(path_to_file) as yaml_file:
+        yaml_dict = yaml.safe_load(yaml_file)
+
+    check_func(yaml_dict)
+    dump_vars_to_file(yaml_dict, output_filename)
